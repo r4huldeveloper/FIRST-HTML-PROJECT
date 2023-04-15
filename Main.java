9f76f0d08065cfd9035fdd4f30db5e026c791614
Rahul Sharma <developerrahul671@gmail.com>
2023-04-15 23:23:58 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+package point;
+
+import java.util.Properties;
+
+public class Main {
+    static void msg(){
+        System.out.println("this is static method");
+
+    }
+    public static void main(String[] args) {
+        // Main m = new Main();
+        // m.msg();
+      
+        System.out.println(System.getProperties());
+        System.out.println(~1);
+    }
+}
