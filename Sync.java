20342da6107917d9b8d804e107fa75bd265977b4
Rahul Sharma <developerrahul671@gmail.com>
2023-04-15 23:23:42 +0530

    Add files via upload

---
+++ Sync.java
@@ -0,0 +1,10 @@
+public class Sync{
+    static void msg(){
+        System.out.println("Hi this is static method");
+    }
+        public static void main(String[] args) {
+            // Sync s = new Sync();
+            // s.msg();
+            Sync.msg();
+        }
+    }
